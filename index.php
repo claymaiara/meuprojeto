736e118778c3e070efe34d7418c5b6273a56ad36
claymaiara <clay_maiara@hotmail.com>
2014-08-02 15:55:19 -0300

    Primeiro Commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+
+header('location: /meuprojeto/modulo');
+
+/* //DIRECTORY_SEPARATOR
+
+$string = '\var\www\stay\system\core\FormControlleroller';
+
+$novastring =  str_replace('\\', '/', $string);
+
+echo $string,PHP_EOL;
+
+echo $novastring;
+ */
+?>
